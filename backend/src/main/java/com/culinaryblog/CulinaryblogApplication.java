b357f2b78b9b48cdc0c035b1ab79fa6e602934a9
Kopyciak4 <grzegorzfoit4@gmail.com>
2019-04-11 23:02:00 +0200

    first commit

---
+++ backend/src/main/java/com/culinaryblog/CulinaryblogApplication.java
@@ -0,0 +1,13 @@
+package com.culinaryblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryblogApplication.class, args);
+    }
+
+}
